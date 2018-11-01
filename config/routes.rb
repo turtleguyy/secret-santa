7ae413ed54f02270da30b5b363cd343fcb8e5011
Zachary Nicoll <github@zdn.me>
2018-11-01 01:27:33 -0500

    All the views!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resources :families
+  unauthenticated do
+    root to: 'devise/sessions#new'
+  end
+
+  authenticated do
+    root to: 'user#show'
+  end
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    resources :families
+  end
 end
